9f469e7650f0508df5abb57af66229470f00c5e0
Baris0 <barisseckin0@gmail.com>
2022-03-08 16:07:49 +0300

    .

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -2,16 +2,27 @@ import { ADD_TO_CART } from "../actions/cartActions";
 import { cartItems } from "../initialValues/cartItems";
 
 const initialState = {
-    cartItem: cartItems
+    cartItems: cartItems
 }
 
 export default function cartReducer(state= initialState, {type, payload}) {
 
     switch (type) {
         case ADD_TO_CART:
-            let product = state.cartItem.find(c => c.product.id === payload.id)
-            break;
-    
+            let product = state.cartItems.find(c => c.product.id === payload.id)
+            if(product) {
+                product.quantitiy++;
+                return {
+                    ...state
+                }
+            }
+            else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems]
+                }
+            }
+
         default:
             break;
     }
